8c2b2b4f3f8a9a2a7cafc901fd1e71b7392ad9ee
schnema123 <marcelschneider5@outlook.de>
2018-02-18 20:03:15 +0100

    Added some more RUS functions

--- DatabaseLibrary/Countries/RUS.cs
+++ DatabaseLibrary/Countries/RUS.cs
@@ -18,8 +18,37 @@ namespace DatabaseLibrary.Countries
           return "переулок";
         case "пр-кт":
           return "проспект";
+        case "пл":
+          return "площадь";
+        case "наб":
+          return "набережная";
       }
       return streetType;
+    }
+
+    // 4-я Северная
+    // 2-я Северная
+    // 5-й Институтский
+    public static string ReplaceStreetName(string streetName)
+    {
+      string[] streetNameParts = streetName.Split(' ');
+      int numParts = streetNameParts.Length;
+
+      if (numParts < 2)
+        return streetName;
+
+      string lastPart = streetNameParts[numParts - 1];
+      if (!lastPart.Contains("-я") &&
+        !lastPart.Contains("-й"))
+        return streetName;
+
+      string[] newParts = new string[numParts];
+
+      newParts[0] = lastPart;
+      for (int i = 0; i < numParts - 1; i++)
+        newParts[i + 1] = streetNameParts[i];
+
+      return String.Join(" ", newParts);
     }
   }
 }
